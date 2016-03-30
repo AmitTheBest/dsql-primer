670b3434fa72c707e53a6feb692188c8ca945a6c
Romans Malinovskis <romans@agiletoolkit.org>
2016-03-30 21:31:20 +0100

    make first iteration work

--- query-demo1.php
+++ query-demo1.php
@@ -30,17 +30,18 @@ echo "\nThe salary record was set by: ";
 
 // Define a query for the smallest salary
 $q_salary = q()
-    ->table('salary')->order('salary desc')->limit(1);
+    ->table('salaries')->order('salary desc')->limit(1);
 
 $q_employee = q()
     // select only employee data with lowest salary
-    ->table('employee')->where('emp_no',(clone $q_salary)->filed('emp_no'))
+    ->table('employees')->where('emp_no',(clone $q_salary)->field('emp_no'))
 
     // additionally select how many months position was taken
-    ->field((clone $q_salary)->field($q_employee->expr('TIMESTAMPDIFF(MONTH, from_date, to_date)')),'months')
-    
+    ->field('*')
+    ->field((clone $q_salary)->field($q_salary->expr('TIMESTAMPDIFF(MONTH, from_date, to_date)')),'months')
+
     ;
 
-
-var_Dump($q_employee->getRow());
+echo json_encode($q_employee->getRow());
+echo "\n\n";
 
